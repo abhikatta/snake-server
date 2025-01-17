f1b90e14836ab1b338a530b08bbfb63ae6e200e5
abhikatta <abhinaykatta97@gmail.com>
2025-01-17 13:16:32 +0530

    some reverts

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var ws_1 = require("ws");
 var food_1 = require("./game-server/food");
 var PORT = process.env.PORT || 3000;
 var server = new ws_1.WebSocketServer({ port: Number(PORT) });
-console.log("WebSocket server started on ws://localhost:".concat(PORT));
+console.log("WebSocket server up!");
 var foodItem = new food_1.Food();
 foodItem.createNewFoodItem();
 server.on("connection", function (socket) {
@@ -21,14 +21,6 @@ server.on("connection", function (socket) {
             }
         });
     });
-    //   server.clients.forEach((client: WebSocket) => {
-    //     if (client !== socket && client.readyState === 1) {
-    //       console.log(
-    //         `Sending food data to client: ${JSON.stringify({ foodData: foodData })}`
-    //       );
-    //       client.send(JSON.stringify(foodData));
-    //     }
-    //   });
     socket.on("close", function () {
         console.log("Client disconnected");
     });
--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { Food } from "./game-server/food";
 const PORT = process.env.PORT || 3000;
 const server = new WebSocketServer({ port: Number(PORT) });
 
-console.log(`WebSocket server started on ws://localhost:${PORT}`);
+console.log(`WebSocket server up!`);
 
 const foodItem = new Food();
 foodItem.createNewFoodItem();
@@ -26,16 +26,6 @@ server.on("connection", (socket: WebSocket) => {
       }
     });
   });
-
-  //   server.clients.forEach((client: WebSocket) => {
-  //     if (client !== socket && client.readyState === 1) {
-  //       console.log(
-  //         `Sending food data to client: ${JSON.stringify({ foodData: foodData })}`
-  //       );
-
-  //       client.send(JSON.stringify(foodData));
-  //     }
-  //   });
 
   socket.on("close", () => {
     console.log("Client disconnected");
